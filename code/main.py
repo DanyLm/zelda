3dc65afabf15ae468d4b5487b65c76ddfcdd97c3
Dany Bitard <dany.bitard@gmail.com>
2022-05-03 17:12:39 +0200

    Creating the UI

---
+++ code/main.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+from settings import *
+from level import Level
+
+
+class Game:
+
+    def __init__(self):
+        # General settings
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Zelda Game')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill("black")
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
